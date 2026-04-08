96ff9ea897e93613393cfae2e7ae4bcf63d33ff2
Sankalp Sagar <sankalpsagar1999@gmail.com>
2026-04-09 02:55:44 +0530

    test: adding failing tests for employees listing

--- spec/requests/api/v1/employees_spec.rb
+++ spec/requests/api/v1/employees_spec.rb
@@ -15,5 +15,34 @@ RSpec.describe "Employees API", type: :request do
 
       expect(response).to have_http_status(:created)
     end
+
+    it "creates an employee in database" do
+      expect {
+        post "/api/v1/employees", params: {
+          employee: {
+            first_name: "John",
+            last_name: "Titor",
+            job_title: "Time Traveler",
+            country: "USA",
+            salary: 50000
+          }
+        }
+      }.to change(Employee, :count).by(1)
+    end
+
+    it "returns a list of employees" do
+      Employee.create!(
+        first_name: "Amitabh",
+        last_name: "Bacchan",
+        job_title: "Actor",
+        country: "India",
+        salary: 70000
+      )
+
+      get "/api/v1/employees"
+
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body).length).to be > 0
+    end
   end
 end
